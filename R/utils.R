38eafc044d4f1ed8b0c790b0be4f6eb4179a7787
Simon Adamov <simon.adamov@mailbox.org>
2023-03-10 12:21:18 +0100

    new license

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 MeteoSwiss, contributors listed in AUTHORS
+
+# Distributed under the terms of the BSD 3-Clause License.
+
+# SPDX-License-Identifier: BSD-3-Clause
+
 #' Sets measurements to NA for all times, where no trap measured anything
 #'
 #' @param data A list of dataframes for the measurements
